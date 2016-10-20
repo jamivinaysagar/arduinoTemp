f1dedcb4fa0a3dffa605aaff8f74c38f528272f2
miksa <miksa007@gmail.com>
2016-10-20 14:08:40 +0300

    basic Lcd Keypad example

---
+++ LCD_Keypad_Test.ino
@@ -0,0 +1,25 @@
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+int x=0;
+ 
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.setCursor(0,0);
+  lcd.print("LCD Key Shield");
+  lcd.setCursor(0,1);
+  lcd.print("Testing");
+
+ 
+}
+
+void loop() {
+  lcd.setCursor(10,1);
+  lcd.print (x);
+  delay(1000);
+  x=x+1;
+}
